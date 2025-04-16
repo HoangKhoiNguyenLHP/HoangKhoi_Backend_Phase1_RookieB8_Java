0899eb594b2ad27360768297585e3fae8e5e8cfe
Nguyen Hoang Khoi <116591477+HoangKhoiNguyenLHP@users.noreply.github.com>
2025-04-16 09:31:36 +0700

    Initialzie project

---
+++ src/main/java/nh/khoi/ecommerce/EcommerceNtApplication.java
@@ -0,0 +1,14 @@
+package nh.khoi.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceNtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceNtApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+}
---
+++ src/test/java/nh/khoi/ecommerce/EcommerceNtApplicationTests.java
@@ -0,0 +1,13 @@
+package nh.khoi.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceNtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
